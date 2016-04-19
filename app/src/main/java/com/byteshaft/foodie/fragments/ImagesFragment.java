da770674120c798e19ac4ef70a1538118fc4e69d
Shahid <shgr8rb@gmail.com>
2016-04-19 11:02:14 +0500

    add img fragment and httpManager class for getting json data

--- app/src/main/java/com/byteshaft/foodie/fragments/ImagesFragment.java
+++ app/src/main/java/com/byteshaft/foodie/fragments/ImagesFragment.java
@@ -1,17 +1,48 @@
 package com.byteshaft.foodie.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.byteshaft.foodie.R;
 
 public class ImagesFragment extends Fragment {
 
+
+    private  View baseView;
+    private  ProgressBar progressBar;
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        baseView = inflater.inflate(R.layout.fragment_image, container, false);
+        progressBar = (ProgressBar) baseView.findViewById(R.id.progress_bar);
+
+        return baseView;
+    }
+
+
+    class GetJsonData extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
